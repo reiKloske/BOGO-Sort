38abee6ff1344d8b371f254edb3eaeb0d0ec1184
reiKloske <lanafofa2@gmail.com>
2024-12-18 19:31:22 +0000

    Improvements
    -Added type hints to the functions for better code clarity and to help with static type checking.
    
    -Added a main guard to allow the script to be imported as a module without executing the code.

--- bogo.py
+++ bogo.py
@@ -1,18 +1,18 @@
 from random import shuffle
 
 # Verifying if array is sorted
-def isSorted(arr):
+def isSorted(arr: list[int]) -> bool:
     for index in range(len(arr) - 1):
         if arr[index] > arr[index + 1]:
             return False
     return True
 
-
 # BOGO Sort
-def bogo(arr):
+def bogo(arr: list[int]) -> None:
     while not isSorted(arr):
         shuffle(arr)
 
-
-wallahi = [4, 2, 3, 5, 1]
-bogo(wallahi)
+if __name__ == "__main__":
+    arr = [4, 2, 3, 5, 1]
+    bogo(arr)
+    print(arr)
